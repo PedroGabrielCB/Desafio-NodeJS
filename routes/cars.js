f9e36910cddb5d3edc4076a17c591245c5f2c760
PedroGabrielCB <contato.pedrogabrielcb@gmail.com>
2023-05-28 14:39:33 -0300

    correção eslint

--- routes/cars.js
+++ routes/cars.js
@@ -2,20 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/mostrar', function(req, res, next) {
-
-    res.send(`O endpoint solicitado foi: ${req.url} e o metodo utilizado foi o ${req.method}`);
+  res.send(`O endpoint solicitado foi: ${req.url} e o metodo utilizado foi o ${req.method}`);
 });
 
 router.post('/adicionar', function(req, res, next) {
-    res.send(`O endpoint solicitado foi: ${req.url} e o metodo utilizado foi o ${req.method}. O carro de ID ${req.body.id} foi adicionado com sucesso!. Dados: ${JSON.stringify(req.body)}`);
+  res.send(`O endpoint solicitado foi: ${req.url} e o metodo utilizado foi o ${req.method}. O carro de ID ${req.body.id} foi adicionado com sucesso!. Dados: ${JSON.stringify(req.body)}`);
 });
 
 router.put('/editar/:id', function(req, res, next) {
-    res.send(`O endpoint solicitado foi: ${req.url} e o metodo utilizado foi o ${req.method}. O carro de ID ${req.params.id} foi atualizado com sucesso!. Dados Atualizados: ${JSON.stringify(req.body)}`);
+  res.send(`O endpoint solicitado foi: ${req.url} e o metodo utilizado foi o ${req.method}. O carro de ID ${req.params.id} foi atualizado com sucesso!. Dados Atualizados: ${JSON.stringify(req.body)}`);
 });
 
 router.delete('/deletar/:id', function(req, res, next) {
-    res.send(`O endpoint solicitado foi: ${req.url},o metodo utilizado foi o ${req.method} e foi solicitado a deleção do carro de ID: ${req.params.id}`);
+  res.send(`O endpoint solicitado foi: ${req.url},o metodo utilizado foi o ${req.method} e foi solicitado a deleção do carro de ID: ${req.params.id}`);
 });
 
 module.exports = router;
